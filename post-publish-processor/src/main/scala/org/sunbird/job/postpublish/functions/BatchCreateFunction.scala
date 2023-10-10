907638e1a5810823505fdb459dda7980080cd01f
anil <anilkumar.kammalapalli@tarento.com>
2023-10-10 10:54:17 +0530

    removed code for adding cert.template to batch

--- post-publish-processor/src/main/scala/org/sunbird/job/postpublish/functions/BatchCreateFunction.scala
+++ post-publish-processor/src/main/scala/org/sunbird/job/postpublish/functions/BatchCreateFunction.scala
@@ -11,19 +11,19 @@ import org.sunbird.job.{BaseProcessFunction, Metrics}
 import java.time.format.DateTimeFormatter
 import java.time.{ZoneId, ZonedDateTime}
 
-class BatchCreateFunction(config: PostPublishProcessorConfig, httpUtil: HttpUtil,@transient var cassandraUtil: CassandraUtil = null)
+class BatchCreateFunction(config: PostPublishProcessorConfig, httpUtil: HttpUtil)
   extends BaseProcessFunction[java.util.Map[String, AnyRef], String](config) with BatchCreation {
 
   private[this] val logger = LoggerFactory.getLogger(classOf[BatchCreateFunction])
 
   override def open(parameters: Configuration): Unit = {
     super.open(parameters)
-    cassandraUtil = new CassandraUtil(config.dbHost, config.dbPort,config)
+    //cassandraUtil = new CassandraUtil(config.dbHost, config.dbPort,config)
   }
 
   override def close(): Unit = {
     super.close()
-    cassandraUtil.close()
+   // cassandraUtil.close()
   }
 
   override def processElement(eData: java.util.Map[String, AnyRef], context: ProcessFunction[java.util.Map[String, AnyRef], String]#Context, metrics: Metrics): Unit = {
@@ -32,7 +32,7 @@ class BatchCreateFunction(config: PostPublishProcessorConfig, httpUtil: HttpUtil
     val startDate = ZonedDateTime.now(ZoneId.of("Asia/Kolkata")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
     logger.info("Creating Batch for " + collectionId + " with start date:" + startDate)
     try {
-      createBatch(eData, startDate)(config, httpUtil, cassandraUtil)
+      createBatch(eData, startDate)(config, httpUtil)
       metrics.incCounter(config.batchCreationSuccessCount)
       logger.info("Batch created for " + collectionId)
     } catch {
